f90cb73574c8c0caeaa40e1e890a43f2416d0d32
SLane35 <SLane35@users.noreply.github.com>
2021-12-01 23:01:18 +0200

    Add files via upload

--- project/lambda.py
+++ project/lambda.py
@@ -31,3 +31,75 @@ def lambda_handler(event, context):
             "inferences": []
         }
     }
+
+
+import json
+import base64
+import os
+import io
+import boto3
+import csv
+
+# Fill this in with the name of your deployed model
+ENDPOINT = 'image-classification-2021-11-29-18-16-35-165'
+runtime= boto3.client('runtime.sagemaker')
+
+def lambda_handler(event, context):
+
+    print("Received event: " + json.dumps(event, indent=2))
+    
+    data = json.loads(json.dumps(event))
+    payload = base64.b64decode(data['image_data'])
+
+    print(payload)
+
+    # Make a prediction:
+    response = runtime.invoke_endpoint(EndpointName=ENDPOINT,
+                                       ContentType='image/png',
+                                       Body=payload)
+    
+        
+    print(response)
+    result = json.loads(response['Body'].read().decode('utf-8'))
+    
+    #pred = int(result['predictions'][0]['score'])
+    #predicted_label = 'M' if pred == 1 else 'B'
+    
+    #inferences = predictor.predict(payload)
+
+    # We return the data back to the Step Function    
+    event["inferences"] = result
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(event)
+    }
+
+import json
+import ast
+
+
+THRESHOLD = .93
+
+
+def lambda_handler(event, context):
+
+    # Grab the inferences from the event
+    inferences = event["inferences"]
+
+    print(inferences)
+    
+    # Check if any values in our inferences are above THRESHOLD
+    meets_threshold = any (i >= THRESHOLD for i in inferences)
+
+    # If our threshold is met, pass our data back out of the
+    # Step Function, else, end the Step Function with an error
+    if meets_threshold:
+        pass
+    else:
+        raise("THRESHOLD_CONFIDENCE_NOT_MET")
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(event)
+    }
